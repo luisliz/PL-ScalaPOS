9c3496520f00730b64c2bb24b9c5f2d17488f758
Gabriel Rosa <gabrosa7@gmail.com>
2019-04-16 23:10:24 -0400

    Allow delete from cart item

--- src/main/scala/shopgui/ShopWindow.scala
+++ src/main/scala/shopgui/ShopWindow.scala
@@ -4,13 +4,20 @@ import java.awt.MediaTracker
 
 import javax.swing.{Icon, ImageIcon}
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Map
 import scala.swing._
 
-class ShopWindow(name: String) extends MainFrame {
+class ShopWindow(name: String) extends MainFrame{
 
   /** To Do:
+    * -GridBag Panel can be used to divide into cart and items sections http://otfried.org/scala/index_42.html
+    * -Section with all items on cart and their amount and total
     * -Only can hit remove from cart button if its on cart (this button could also be next to the item on the cart section)
+    * -[DONE] Lower inventory with every click and restore it if item removed from cart
+    * -[DONE] Only can add to cart if enough inventory
+    * -[DONE]Pass info to the receipt
+    * -[DONE]Label that says the amount there is of an item in the cart currently (thinking of making it a var in Item class)
+    * -[DONE]add images for items and change on Item class (currently images are set as strings)
     * */
 
   /** Constructor if no name is passed */
@@ -18,7 +25,7 @@ class ShopWindow(name: String) extends MainFrame {
 
   //  private var itemList = List[Item]()
   // Just testing :)
-  private var itemList = List(new Item("idk", "coca-cola", "coa-cola.jpg", 10, 0.99),
+  private var itemList = List(new Item("idk", "coca-cola", "coca-cola.jpg", 10, 0.99),
     new Item("idk", "arroz", "arroz.png", 54, 2.99),
     new Item("idk", "pizza", "pizza.png", 49, 14.99),
     new Item("idk", "coco", "coco.jpg", 11, 2.99),
@@ -34,47 +41,24 @@ class ShopWindow(name: String) extends MainFrame {
     "This is the default receiptFooter. To change it type the command 'receiptFooter: [new footer]' in the console."
 
   /* Items in cart */
-  private var cart: Map[Item, Int] = Map()
+  private var cart:Map[Item,Int] = Map()
 
   /* Panel containing the cart, will be updated after entries */
   private val cartPanel = new BoxPanel(Orientation.Vertical) {
-    contents += new BoxPanel(Orientation.Horizontal) {}
-  }
-  /** Left Panel - Product Display*/
-  var itemPanel = new FlowPanel() {
-    for (i <- itemList) {
-      contents += new BoxPanel(Orientation.Vertical) {
-        var icon = new ImageIcon("src/media/products/" + i.photo)
-        if(icon.getImageLoadStatus != MediaTracker.COMPLETE) icon = new ImageIcon("src/media/products/no-image.png")
-        var addToCartButton = Button("") { addToCart(i, this) }
-        addToCartButton.enabled_=(i.inventory > 0)
-        addToCartButton.icon_=(icon)
-        //          addToCartButton.maximumSize_=(new Dimension(100, 100))
-        contents += addToCartButton
-        contents += Swing.VStrut(5)
-        contents += new Label(i.name)
-        contents += Swing.VStrut(5)
-        contents += new Label("$" + i.price)
-        contents += Swing.VStrut(5)
-        contents += new Label("Amount left: " + i.inventory)
-        var removeCartButton = Button("Remove from cart") { removeFromCart(i, this) }
-        removeCartButton.enabled_=(false)
-        contents += removeCartButton
-        border = Swing.MatteBorder(1, 1, 1, 1, java.awt.Color.BLACK)
-      }
-    }
+      contents += new BoxPanel(Orientation.Horizontal) {}
   }
 
   /* MainFrame class actions */
   title = name
-  preferredSize = new Dimension(1000, 600)
+  preferredSize = new Dimension(800, 500)
 
   private var totalLabel = new Label("Total: $" + transactionTotal)
 
-  contents = new BorderPanel {
-    add(itemPanel , BorderPanel.Position.Center)
+  private val mainPanel = new BorderPanel {
+    /** Left Panel - Product Display*/
+    add(getItemsPanel(), BorderPanel.Position.Center)
 
-    /** Right Panel - Invoice Display */
+    /** Right Panel - Invoice Display*/
     var invoice = new BorderPanel {
       add(cartPanel, BorderPanel.Position.Center)
 
@@ -83,9 +67,11 @@ class ShopWindow(name: String) extends MainFrame {
         contents += Button("Checkout") { checkout() }
       }, BorderPanel.Position.South)
     }
-    invoice.background = new Color(255, 255, 255) //set background white
+    invoice.background = new Color(255,255,255) //set background white
     add(invoice, BorderPanel.Position.East)
   }
+
+  contents = mainPanel
 
   /** Change main screen name */
   def renameShop(newName: String): Unit = {
@@ -112,26 +98,18 @@ class ShopWindow(name: String) extends MainFrame {
 
   /** Here you would pass the totals and the name of the items to pass to the receipt and then clear them for the next transaction */
   private def checkout(): Unit = {
-    if(cart.size > 0) {
+    if(cart.size > 0)
       displayReceipt()
       transactionTotal = 0
-      totalLabel.text_=("Total: $" + f"$transactionTotal%1.2f")
-      cart = cart.empty
-      updateCart()
-//      for (c <- contents) {
-//        c.repaint()
-//      }
-    }
   }
 
   private def displayReceipt(): Unit = {
     var items = new StringBuilder()
     for ((k,v) <- cart) {
-      items ++= k.name + "\t\t\t (" + v + " * $" + k.price + ") \t\t = \t\t $" + (v*k.price).toString + "\n"
+      items ++= k.name + " (quantity: " + v.toString + "   price: " + k.price.toString + ")\n"
     }
 
-    Dialog.showMessage(contents.head, receiptHeader + "\n\n\n" + items + "\n\n\nTotal: " + transactionTotal
-      + "\n\n" + receiptFooter, title="Receipt")
+    Dialog.showMessage(contents.head, receiptHeader + "\n\n\n" + items + "\n\n" + receiptFooter, title="Receipt")
   }
 
   private def addToCart(item: Item, boxPanel: BoxPanel): Unit = {
@@ -148,15 +126,7 @@ class ShopWindow(name: String) extends MainFrame {
     }
 
     //add item to cart collection (includes quantity as map value)
-    if (cart.contains(item)) cart += (item -> (cart(item) + 1)) else (cart += (item -> 1))
-
-    // disable remove button if necessary
-    if (cart.contains(item)) {
-      val newButton = Button("Remove from cart") { removeFromCart(item, boxPanel) }
-      newButton.enabled_=(true)
-      boxPanel.contents.update(7, newButton)
-      boxPanel.repaint()
-    }
+    if(cart.contains(item)) cart += (item -> (cart(item)+1)) else (cart += (item -> 1))
 
     //update displayed cart
     updateCart()
@@ -166,39 +136,22 @@ class ShopWindow(name: String) extends MainFrame {
     //    println(item.inventory)
   }
 
-  private def updateCart(): Unit = {
+  private def updateCart(): Unit ={
     val p = new ScrollPane(new BoxPanel(Orientation.Vertical) {
 //      println(cart)
       for ((k,v) <- cart) {
         println(s"key: $k, value: $v")
         contents += new BoxPanel(Orientation.Horizontal) {
           contents += Swing.HStrut(10)
-          contents += Button("x") { removeFromCart2(k) }
+          contents += Button("X") { removeFromCart(k) }
           contents += Swing.HStrut(10)
-          contents += new Label(k.name + " (" + v + " * $" + k.price + ") = $" + (v*k.price).toString)
+          contents += new Label(k.name + " (" + v + " * " + k.price + ")")
           contents += Swing.HStrut(10)
         }
       }
     })
     (cartPanel.contents).update(0, p)
     cartPanel.repaint()
-//    cartPanel.revalidate()
-  }
-
-  /** should replace  removeFromCart since it is now only done from the cart section but cant get it to
-    * update the item section since last method had the item's boxPanel as parameter
-    */
-  private def removeFromCart2(item: Item): Unit = {
-    println(contents(0).toString() + "\n")
-    if(cart.contains(item)) {
-      if (cart(item) == 1)
-        cart -= (item)
-      else
-        cart += (item -> (cart(item) - 1))
-    }
-    transactionTotal = transactionTotal - item.price
-    totalLabel.text_=("Total: $" + f"$transactionTotal%1.2f")
-    updateCart()
   }
 
   private def addToTotal(amountToAdd: Double): Unit = {
@@ -207,42 +160,24 @@ class ShopWindow(name: String) extends MainFrame {
     println("transactionTotal: " + transactionTotal)
   }
 
-  private def removeFromCart(item: Item, boxPanel: BoxPanel): Unit = {
+  private def removeFromCart(item: Item): Unit = {
     // update item inventory
     removeFromTotal(item.price)
     item.addInventory(1)
     println("inventory: " + item.inventory)
 
-    // enable button if necessary
-    if (item.inventory > 0) {
-      val newButton = boxPanel.contents.head
-      newButton.enabled_=(true)
-      boxPanel.contents.update(0, newButton)
-    }
-
-    //add item to cart collection (includes quantity as map value)
-    if (cart.contains(item)) {
+    //remove item to cart collection (includes quantity as map value)
+    if(cart.contains(item)) {
       if (cart(item) == 1)
         cart -= (item)
       else
         cart += (item -> (cart(item) - 1))
     }
 
-    // disable remove button if necessary
-    if (!cart.contains(item)) {
-      val newButton = Button("Remove from cart") { removeFromCart(item, boxPanel) }
-      newButton.enabled_=(false)
-      boxPanel.contents.update(7, newButton)
-      boxPanel.repaint()
-    }
-
     //update displayed cart
     updateCart()
 
-    // update amount left label
-    updateAmountLabel(item, boxPanel)
-    //    println(item.inventory)
-
+    mainPanel.layout.update(getItemsPanel(), BorderPanel.Position.Center)
   }
 
   /* This doesn't have a condition for if the total gets to be less than 0 because the button should only be clickable with items that
@@ -260,5 +195,26 @@ class ShopWindow(name: String) extends MainFrame {
     boxPanel.repaint()
   }
 
-
+  private def getItemsPanel(): FlowPanel ={
+    new FlowPanel() {
+      for (i <- itemList) {
+        contents += new BoxPanel(Orientation.Vertical) {
+          var icon = new ImageIcon("src/media/products/" + i.photo)
+          if(icon.getImageLoadStatus != MediaTracker.COMPLETE) icon = new ImageIcon("src/media/products/no-image.png")
+          var addToCartButton = Button("") { addToCart(i, this) }
+          addToCartButton.enabled_=(i.inventory > 0)
+          addToCartButton.icon_=(icon)
+          //          addToCartButton.maximumSize_=(new Dimension(100, 100))
+          contents += addToCartButton
+          contents += Swing.VStrut(5)
+          contents += new Label(i.name)
+          contents += Swing.VStrut(5)
+          contents += new Label("$" + i.price)
+          contents += Swing.VStrut(5)
+          contents += new Label("Amount left: " + i.inventory)
+          border = Swing.MatteBorder(1, 1, 1, 1, java.awt.Color.BLACK)
+        }
+      }
+    }
+  }
 }
