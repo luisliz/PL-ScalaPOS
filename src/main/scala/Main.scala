4d509bcacc05658e73bf3698b42c00e127d3ee80
Luis Liz <luis.liz@upr.edu>
2019-04-16 20:39:00 -0400

    Expanding grammar

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,21 @@ import shopgui.ShopWindow
 object Main {
   def main(args: Array[String]) {
     val gui = new ShopWindow
-    gui.visible = true
+    gui.visible = false
     println("End of main function")
+
+    val validCode =
+    """
+    createShop: "Hey";
+    addItem: "food", "arroz", "arroz.jpg", 300, 1.50;
+    """.stripMargin.trim;
+
+
+    //
+    //addItem: "food", "arroz", arroz.jpg, 300, 1.50
+
+    println("LEXER: " + POSLexer.apply(validCode).toString)
+    println("Compiler: " + POSCompiler.apply(validCode).toString)
+    //println(POSCompiler.apply(validCode).toString)
   }
 }
