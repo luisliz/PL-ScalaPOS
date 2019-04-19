340cc608d9d7b3c5e3e1c9061b60b79264a47262
Luis Liz <luis.liz@upr.edu>
2019-04-19 16:59:20 -0400

    Set all parsing so its ready for other functions

--- src/main/scala/LexThisParseThat.scala
+++ src/main/scala/LexThisParseThat.scala
@@ -3,116 +3,157 @@ import shopgui.{Item, ShopWindow}
 import scala.util.parsing.combinator.RegexParsers
 
 class LexThisParseThat extends RegexParsers {
+	override def skipWhitespace = true
 
-	val string = "[A-Za-z]+".r
+	val double = """[0-9]*[.]?[0-9]+""".r
 
-	val comilla = "\"".r
+	val digit = """[0-9_]+""".r
+
+	val string =
+		""""(.*?)"""".r ^^ { str =>
+			val content = str.substring(1, str.length - 1).toString
+			content
+		}
 
 	var gui = new ShopWindow
 	gui.visible = false
 
 	def program: Parser[Any] = createShop ~ opt(rep(expr))
 
-	def expr: Parser[Any] = InvExp //| ReceiptExp | AccExp
+	def expr: Parser[Any] = ShopExp | InvExp | ReceiptExp | AccExp
 
-	def createShop: Parser[Any] = "createShop" ~ opt(":" ~ comilla ~ string ~ comilla) ^^ {
+	def createShop: Parser[Any] = "createShop" ~ opt(":" ~ string) ^^ {
 		case a ~ None => {
 			gui.visible = true
 		}
 
-		case a ~ Some(":" ~ "\"" ~ b ~ "\"") => {
+		case a ~ Some(":" ~ b) => {
 			gui = new ShopWindow(b)
 			//      gui.addItem(new Item("food", "food", "coco.jpg", 10, 20))
 			gui.visible = true
 		}
 	}
 
+	def ShopExp: Parser[Any] = {
+		def renameShop: Parser[Any] = "renameShop" ~ ":" ~ string ^^ {
+			case _ ~ b => {
+				gui.renameShop(b)
+			}
+		}
+
+		renameShop
+	}
+
 	def InvExp: Parser[Any] = {
-		def addItem: Parser[Any] = "addItem" ~ ":" ~ string ~ "," ~ string ~ "," ~ string ~ "," ~ string ~ "," ~ string ^^ {
-			case a ~ b ~ c ~ d ~ e => {
-				print("add item")
+		def addItem: Parser[Any] = "addItem" ~ ":" ~ string ~ "," ~ string ~ "," ~ string ~ "," ~ digit ~ "," ~ double ^^ {
+			case _ ~ category ~ _ ~ itemName ~ _ ~ imageName ~ _ ~ units ~ _ ~ price => {
+				println("addItem: " + category + " " + itemName + " " + imageName + " " + units + " " + price)
 			}
 		}
 
 		def deleteItem: Parser[Any] = "deleteItem" ~ ":" ~ string ^^ {
-			case a => {
-				println("deleteItem")
+			case _ ~ a => {
+				println("deleteItem: " + a)
 			}
 		}
 
-		def updateInventory: Parser[Any] = "updateInventory" ~ ":" ~ string ~ "," ~ string ^^ {
-			case a ~ b => {
-				println("updateInventory")
+		def updateInventory: Parser[Any] = "updateInventory" ~ ":" ~ string ~ "," ~ digit ^^ {
+			case _ ~ itemName ~ _ ~ amount => {
+				println("updateInventory:" + itemName + " " + amount)
 			}
 		}
 
-		def addInventory: Parser[Any] = "addInventory" ~ ":" ~ string ~ "," ~ string ^^ {
-			case a ~ b => {
-				println("addinventory")
+		def addInventory: Parser[Any] = "addInventory" ~ ":" ~ string ~ "," ~ digit ^^ {
+			case _ ~ itemName ~ _ ~ amount => {
+				println("addinventory: " + itemName + " " + amount)
 			}
 		}
 
-		def removeInventory: Parser[Any] = "removeInventory" ~ ":" ~ string ~ "," ~ string ^^ {
-			case a ~ b => {
-				println("removeInventory")
+		def removeInventory: Parser[Any] = "removeInventory" ~ ":" ~ string ~ "," ~ digit ^^ {
+			case _ ~ itemName ~ _ ~ amount => {
+				println("removeInventory: " + itemName + " " + amount)
 			}
 		}
 
-		def updatePrice: Parser[Any] = "updatePrice" ~ ":" ~ string ~ "," ~ string ^^ {
-			case a ~ b => {
-				println("updatePrice")
+		def updatePrice: Parser[Any] = "updatePrice" ~ ":" ~ string ~ "," ~ double ^^ {
+			case _ ~ itemName ~ _ ~ newPrice => {
+				println("updatePrice: " + itemName + " " + newPrice)
 			}
 		}
 
 		def updateCategory: Parser[Any] = "updateCategory" ~ ":" ~ string ~ "," ~ string ^^ {
-			case a ~ b => {
-				println("updateCategory")
+			case _ ~ a ~ _ ~ b => {
+				println("updateCategory" + a + " " + b)
 			}
 		}
 
 		def updatePhoto: Parser[Any] = "updatePhoto" ~ ":" ~ string ~ "," ~ string ^^ {
-			case a ~ b => {
-				println("updatePhoto")
+			case _ ~ a ~ _ ~ b => {
+				println("updatePhoto" + a + " " + b)
 			}
 		}
 
 		def settlementGridDimensions: Parser[Any] = "settlementGridDimensions" ~ ":" ~ string ~ "," ~ string ^^ {
-			case a ~ b => {
-				println("settlementGridDimensions")
+			case _ ~ a ~ _ ~ b => {
+				println("settlementGridDimensions" + a + " " + b)
 			}
 		}
 
 		def addToCart: Parser[Any] = "addToCart" ~ ":" ~ string ~ "," ~ string ^^ {
-			case a ~ b => {
-				println("addToCart")
+			case _ ~ a ~ _ ~ b => {
+				println("addToCart" + a + " " + b)
 			}
 		}
 
 		def removeToCart: Parser[Any] = "removeToCart" ~ ":" ~ string ~ "," ~ string ^^ {
-			case a ~ b => {
-				println("removeToCart")
+			case _ ~ a ~ _ ~ b => {
+				println("removeToCart" + a + " " + b)
 			}
 		}
 
 		addItem | deleteItem | updateInventory | addInventory | removeInventory | updatePrice | updateCategory | updatePhoto | settlementGridDimensions | addToCart | removeToCart
 	}
 
+	def ReceiptExp: Parser[Any] = {
+		def receiptHeader: Parser[Any] = "receiptHeader" ~ ":" ~ string ^^ {
+			case a => {
+				println("receiptHeader" + a)
+				//gui.changeReceiptHeader(str)
+			}
+		}
 
-	/*def ReceiptExp: Parser[Any] =
+		def receiptFooter: Parser[Any] = "receiptFooter:" ~ string ^^ {
+			case a => {
+				println("receiptFooter" + a)
+				//gui.changeReceiptFooter(str)
+			}
+		}
 
-	def AccExp: Parser[Any] =*/
+		def deleteHeader: Parser[Any] = "deleteHeader" ^^ {
+			case _ => {
+				println("deleteHeader")
+				//gui.changeReceiptHeader("")
+			}
+		}
 
-	/*
-		def addItem
+		def deleteFooter: Parser[Any] = "deleteFooter" ^^ {
+			case _ => {
+				println("deleteFooter")
+				//gui.changeReceiptFooter("")
+			}
+		}
 
-		def receiptHeader
+		receiptHeader | receiptFooter | deleteHeader | deleteFooter
+	}
 
-		def receiptFooter
+	def AccExp: Parser[Any] = {
+		def addUser: Parser[Any] = "addUser:" ~ string ^^ {
+			case str => {
+				print("addUser" + str)
+				//gui.addUser(str) falta hacer esta funcion en ShopWindow. se me ocurre hacer like a label somewhere que diga cual es el current user and maybe algo have like a button to change the current user (like literalmente who is using the gui) and also include the user info on the receipt (like fuiste atendido por tal user)
+			}
+		}
 
-		def deleteHeader
-
-		def deleteFooter*/
-
-	//def accExp:
-
+		addUser
+	}
 }
