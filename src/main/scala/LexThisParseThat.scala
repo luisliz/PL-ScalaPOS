3068477e2b82f8a8f31b2a3147e0a0b74e637d43
Luis Liz <luis.liz@upr.edu>
2019-04-19 01:45:00 -0400

    Done invExpressions and general expression

--- src/main/scala/LexThisParseThat.scala
+++ src/main/scala/LexThisParseThat.scala
@@ -4,49 +4,115 @@ import scala.util.parsing.combinator.RegexParsers
 
 class LexThisParseThat extends RegexParsers {
 
-  val string = "[A-Za-z]+".r
+	val string = "[A-Za-z]+".r
 
-  val comilla = "\"".r
+	val comilla = "\"".r
 
-  var gui = new ShopWindow
-  gui.visible = false
+	var gui = new ShopWindow
+	gui.visible = false
 
-  def expr: Parser[Any] =  createShop ~ opt(rep(expr)) | addItem | receiptHeader | receiptFooter | deleteHeader | deleteFooter
+	def program: Parser[Any] = createShop ~ opt(rep(expr))
 
-  def createShop: Parser[Any] = "createShop" ~ opt(":" ~ comilla ~ string ~ comilla) ^^ {
+	def expr: Parser[Any] = InvExp //| ReceiptExp | AccExp
 
-    case a ~ None => {
-      gui.visible = true
-    }
+	def createShop: Parser[Any] = "createShop" ~ opt(":" ~ comilla ~ string ~ comilla) ^^ {
+		case a ~ None => {
+			gui.visible = true
+		}
 
-    case a ~ Some(":" ~ "\"" ~ b ~ "\"") => {
-      gui = new ShopWindow(b)
-      //      gui.addItem(new Item("food", "food", "coco.jpg", 10, 20))
-      gui.visible = true
-    }
-  }
+		case a ~ Some(":" ~ "\"" ~ b ~ "\"") => {
+			gui = new ShopWindow(b)
+			//      gui.addItem(new Item("food", "food", "coco.jpg", 10, 20))
+			gui.visible = true
+		}
+	}
 
-//  def renameShop: Parser[Any] = "renameShop:" ~ comilla ~ string ~ comilla ^^ {
-//    case "renameShop:" ~ "\"" ~ b ~ "\"" => {
-//      gui.renameShop(b)
-//    }
-//  }
+	def InvExp: Parser[Any] = {
+		def addItem: Parser[Any] = "addItem" ~ ":" ~ string ~ "," ~ string ~ "," ~ string ~ "," ~ string ~ "," ~ string ^^ {
+			case a ~ b ~ c ~ d ~ e => {
+				print("add item")
+			}
+		}
 
-  def addItem
+		def deleteItem: Parser[Any] = "deleteItem" ~ ":" ~ string ^^ {
+			case a => {
+				println("deleteItem")
+			}
+		}
 
-  def receiptHeader
+		def updateInventory: Parser[Any] = "updateInventory" ~ ":" ~ string ~ "," ~ string ^^ {
+			case a ~ b => {
+				println("updateInventory")
+			}
+		}
 
-  def receiptFooter
+		def addInventory: Parser[Any] = "addInventory" ~ ":" ~ string ~ "," ~ string ^^ {
+			case a ~ b => {
+				println("addinventory")
+			}
+		}
 
-  def deleteHeader
+		def removeInventory: Parser[Any] = "removeInventory" ~ ":" ~ string ~ "," ~ string ^^ {
+			case a ~ b => {
+				println("removeInventory")
+			}
+		}
 
-  def deleteFooter
+		def updatePrice: Parser[Any] = "updatePrice" ~ ":" ~ string ~ "," ~ string ^^ {
+			case a ~ b => {
+				println("updatePrice")
+			}
+		}
 
-  def deleteItem: Parser[Any] = "deleteItem:" ~ comilla ~ string ~ comilla ^^ {
-    case "deleteItem:" ~ "\"" ~ b ~ "\"" => {
-      //ShopWindow functions need to be rewritten to accept the name of the item
-//      gui.removeItem(b)
-    }
-  }
+		def updateCategory: Parser[Any] = "updateCategory" ~ ":" ~ string ~ "," ~ string ^^ {
+			case a ~ b => {
+				println("updateCategory")
+			}
+		}
+
+		def updatePhoto: Parser[Any] = "updatePhoto" ~ ":" ~ string ~ "," ~ string ^^ {
+			case a ~ b => {
+				println("updatePhoto")
+			}
+		}
+
+		def settlementGridDimensions: Parser[Any] = "settlementGridDimensions" ~ ":" ~ string ~ "," ~ string ^^ {
+			case a ~ b => {
+				println("settlementGridDimensions")
+			}
+		}
+
+		def addToCart: Parser[Any] = "addToCart" ~ ":" ~ string ~ "," ~ string ^^ {
+			case a ~ b => {
+				println("addToCart")
+			}
+		}
+
+		def removeToCart: Parser[Any] = "removeToCart" ~ ":" ~ string ~ "," ~ string ^^ {
+			case a ~ b => {
+				println("removeToCart")
+			}
+		}
+
+		addItem | deleteItem | updateInventory | addInventory | removeInventory | updatePrice | updateCategory | updatePhoto | settlementGridDimensions | addToCart | removeToCart
+	}
+
+
+	/*def ReceiptExp: Parser[Any] =
+
+	def AccExp: Parser[Any] =*/
+
+	/*
+		def addItem
+
+		def receiptHeader
+
+		def receiptFooter
+
+		def deleteHeader
+
+		def deleteFooter*/
+
+	//def accExp:
 
 }
