2fc4e5a34afc10c1500f7021daa50e5fa73cee6d
luisrampolla <luis.rampolla@upr.edu>
2019-04-19 21:46:35 -0400

    Add recurring commands in main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -28,6 +28,15 @@ object Main {
 
         val result = parser.parseAll(parser.program, shortFile)
 
-        println(result.get)
+    println(result.get)
+
+    var flag = true
+
+    while (flag) {
+      println("Enter command:")
+      val input: String = scala.io.StdIn.readLine()
+      if (input == "quit") { flag = false }
+      else { val toParse = parser.parseAll(parser.expr, input) }
     }
+  }
 }
